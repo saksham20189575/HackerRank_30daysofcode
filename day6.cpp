e221586417edde67f07ab134fb6a04b29b68c1e4
saksham20189575 <42668221+saksham20189575@users.noreply.github.com>
2018-10-04 13:50:03 +0530

    Improved logic of code

--- day6.cpp
+++ day6.cpp
@@ -1,41 +1,36 @@
 /*
-Given a string,S , of length N that is indexed from 0 to N-1, print its even-indexed and odd-indexed characters as 
+Given a string,S , of length N that is indexed from 0 to N-1, print its even-indexed and odd-indexed characters as
 space-separated strings on a single line.
 
 Input Format
-The first line contains an integer, t (the number of test cases)(1<=t<=10). 
+The first line contains an integer, t (the number of test cases)(1<=t<=10).
 Each line i of the t subsequent lines contain a String,S .
 length of S(2 to 10000).
 */
 
-//#include <cmath>
-//#include <cstdio>
-//#include <vector>
-#include <iostream>
-//#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
-
-
 int main() {
     int t;
     cin>>t;
-    for(int i=0;i<t;i++)
+    while(t--)
     {
-        string s,s1,s2;
+        string s,s1="",s2="";
         cin>>s;
-        for(int j=0;j<s.size();j++)
+        for(int j=0;j<s.size();++j)//++j reduces time
         {
-            if(j%2==0){
+            if(j%2==0)
+            {
                 s1+=s[j];
             }
-            else{
+            else
+            {
                 s2+=s[j];
             }
         }
         cout<<s1<<" "<<s2;
-        cout<<"\n";
+        if(t!=0)
+           cout<<"\n";
     }
-    
-    
     return 0;
 }
